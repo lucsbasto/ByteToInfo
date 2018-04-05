c728617a77a4aa32caa79a3971a95b2b2f5862f2
Lucas Bastos <lucsbasto@gmail.com>
2018-04-05 16:10:18 -0300

    add tamanho e assinatura png

--- imagem-to-byte.py
+++ imagem-to-byte.py
@@ -1,7 +1,16 @@
-file = open('imagem.jpg', 'rb');
-file1 = open('teste.txt', 'wb');
+png = {};
+file = open('imagem.png', 'rb');
+data = open('teste.txt', 'wb');
 text = file.readlines();
 for i in text:
-    file1.write(i);
-file1.close()
+    data.write(i);
+len_bytes = file.tell();
+png['tamanho'] = len_bytes;
+for i in range(len_bytes):
+    file.seek(i);
+    oito_bytes = file.read(8);
+    if oito_bytes == b"\x89\x50\x4e\x47\x0d\x0a\x1a\x0a": #8 primeiros bytes do PNG
+        png['tipo'] = 'PNG'
+
+data.close()
 file.close()
