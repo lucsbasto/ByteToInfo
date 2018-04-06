11dc547834f62b1b0bcbd6edfc6b9acf067e1780
Lucas Bastos <lucsbasto@gmail.com>
2018-04-05 21:13:39 -0300

    informação de imagem jpg

--- byte-to-info-jpg.py
+++ byte-to-info-jpg.py
@@ -1,9 +1,6 @@
 jpg = {};
-file = open('imagem.jpg', 'rb');
-data = open('teste.txt', 'wb');
-text = file.readlines();
-for i in text:
-    data.write(i);
+file = open('imagem.jpeg', 'rb');
+bytes = bytes(file.read());
 len_bytes = file.tell();
 for i in range(len_bytes):
     file.seek(i);
@@ -11,15 +8,20 @@ for i in range(len_bytes):
     if b'\xff\xd8' in dois_bytes:
         jpg['Tipo']='JPEG';
         jpg['Tamanho'] = len_bytes;
-
-        # marker = int.from_bytes(file.read(2), byteorder='big', signed=False); #marcação do jpeg que não sei pra que serve
-        # lenght = int.from_bytes(file.read(2), byteorder='big', signed=False); #tamanho do segmento, que tb não sei o que é aeuhaeuhae
-        # identify = file.read(5) #int.from_bytes(file.read(5), byteorder='big', signed=False); # pegando o identificador => JFIF
-        # version = int.from_bytes(file.read(2), byteorder='big', signed=False); #versão do JFIF
-        # units = file.read(1); # 00 sem unidade, 01 pixel por inch 2,54cm(acho que é polegada), 02 pixel por cm
-        # jpg['marker']=marker;
-data.close()
+        marker = file.read(2); #marcação do jpeg que não sei pra que serve
+        jpg['marker'] = marker
+        lenght = int.from_bytes(file.read(2), byteorder='big', signed=False); #tamanho do segmento, que tb não sei o que é aeuhaeuhae
+        jpg['tamanho do campo APP0'] = lenght
+        identify = file.read(5);
+        jpg['identificador'] = identify
+        version = int.from_bytes(file.read(2), byteorder='big', signed=False);
+        jpg['version'] = version
+        unit = int.from_bytes(file.read(1), byteorder='big', signed=False);
+        print (unit)
+        height = int.from_bytes(file.read(2), byteorder='big', signed=False);
+        print (height)
+        width = int.from_bytes(file.read(2), byteorder='big', signed=False);
+        print (width)
 file.close()
-
 print (jpg)
 # {'tamanho': 348527, 'tipo': 'PNG', 'width': 580, 'height': 604, 'profundidade de bit': 8, 'tipo da cor': 6, 'metodo de filtro': 0}
