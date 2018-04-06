f8435f9b8fed4bcd51e4e1462944c0ead1dd702d
qnedy <altmaisefequatro@gmail.com>
2018-04-05 22:38:40 -0300

    Final

--- JPG-Info-Extracto.py
+++ JPG-Info-Extracto.py
@@ -1,3 +1,18 @@
+def getMarker():
+    marker = file.read(2);
+    return marker
+
+def getLenght():
+    lenght = int.from_bytes(file.read(2), byteorder='big', signed=False);
+    return lenght
+
+def getIdentify():
+    identify = file.read(5);
+    return identify
+
+def getVersion():
+    version = int.from_bytes(file.read(2), byteorder='big', signed=False);
+    return version
 
 jpg = {};
 file = open('imagem.jpg', 'rb');
@@ -8,21 +23,23 @@ for i in range(len_bytes):
     dois_bytes = file.read(2);
     if b'\xff\xd8' in dois_bytes:
         
+             
+        marker = getMarker();
+        lenght = getLenght();
+        identify = getIdentify();
+        version = getVersion();
         
-        marker = file.read(2); #marcação do jpeg que não achamos pra que serve
-        jpg['marker'] = marker
-        lenght = int.from_bytes(file.read(2), byteorder='big', signed=False); #tamanho do segmento, que tb não sei o que é aeuhaeuhae
-        jpg['tamanho do campo APP0'] = lenght
-        identify = file.read(5);
-        jpg['identificador'] = identify
-        version = int.from_bytes(file.read(2), byteorder='big', signed=False);
-        jpg['version'] = version
+        
         unit = int.from_bytes(file.read(1), byteorder='big', signed=False);
         
         jpg['Tipo']='JPEG';
         jpg['Tamanho'] = len_bytes;
+        jpg['marker'] = marker
+        jpg['tamanho do campo APP0'] = lenght
+        jpg['identificador'] = identify
+        jpg['version'] = version
        
 
 file.close()
 print (jpg)
-# {'tamanho': 348527, 'tipo': 'PNG', 'width': 580, 'height': 604, 'profundidade de bit': 8, 'tipo da cor': 6, 'metodo de filtro': 0}
+
