f1a8af3bf6358368d6753b91d06652110f6c9573
qnedy <altmaisefequatro@gmail.com>
2018-04-05 22:13:35 -0300

    Finalizando

--- JPG-Info-Extracto.py
+++ JPG-Info-Extracto.py
@@ -1,14 +1,15 @@
+
 jpg = {};
-file = open('imagem.jpeg', 'rb');
+file = open('imagem.jpg', 'rb');
 bytes = bytes(file.read());
 len_bytes = file.tell();
 for i in range(len_bytes):
     file.seek(i);
     dois_bytes = file.read(2);
     if b'\xff\xd8' in dois_bytes:
-        jpg['Tipo']='JPEG';
-        jpg['Tamanho'] = len_bytes;
-        marker = file.read(2); #marcação do jpeg que não sei pra que serve
+        
+        
+        marker = file.read(2); #marcação do jpeg que não achamos pra que serve
         jpg['marker'] = marker
         lenght = int.from_bytes(file.read(2), byteorder='big', signed=False); #tamanho do segmento, que tb não sei o que é aeuhaeuhae
         jpg['tamanho do campo APP0'] = lenght
@@ -17,11 +18,11 @@ for i in range(len_bytes):
         version = int.from_bytes(file.read(2), byteorder='big', signed=False);
         jpg['version'] = version
         unit = int.from_bytes(file.read(1), byteorder='big', signed=False);
-        print (unit)
-        height = int.from_bytes(file.read(2), byteorder='big', signed=False);
-        print (height)
-        width = int.from_bytes(file.read(2), byteorder='big', signed=False);
-        print (width)
+        
+        jpg['Tipo']='JPEG';
+        jpg['Tamanho'] = len_bytes;
+       
+
 file.close()
 print (jpg)
 # {'tamanho': 348527, 'tipo': 'PNG', 'width': 580, 'height': 604, 'profundidade de bit': 8, 'tipo da cor': 6, 'metodo de filtro': 0}
